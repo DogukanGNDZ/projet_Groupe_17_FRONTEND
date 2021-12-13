7473652921efd2e6282315a7f60dbf3df4b199a8
Jérôme Devaux <jerome.devaux@student.vinci.be>
2021-12-13 09:53:40 +0100

    About us Page changed

--- js-phaser-boilerplate/src/Components/Pages/AboutUsPage.js
+++ js-phaser-boilerplate/src/Components/Pages/AboutUsPage.js
@@ -2,11 +2,12 @@ import { RedirectUrl } from "../Router/Router";
 
 let aboutUsPage = `<div class="container-fluid panneau-aboutus">
   <ul class="cadre-panneau-aboutus">
-    <li>a définir</li>
-    <li> a complèter</li>
-    <li> a complèter</li>
-    <li> a complèter</li>     
-    <li> a complèter</li>
+    <li>We are a group of 4 students from Paul Lambin Institute (<a href ="https://www.vinci.be/fr/">IPL</a>). As part of the Web2 course, we were meant to create a website using JS.
+    We decided to create a small game to provide entertainment. The goal is to collect as much coins as possible and put them in a safety chest.</li>
+    <li>dogukan.gunduz@student.vinci.be</li>
+    <li>maxime.ferriere@student.vinci.be</li>
+    <li>mateusz.krawczykowicz@student.vinci.be</li>     
+    <li>jerome.devaux@student.vinci.be</li>
   </ul>
   </div>`;
 
