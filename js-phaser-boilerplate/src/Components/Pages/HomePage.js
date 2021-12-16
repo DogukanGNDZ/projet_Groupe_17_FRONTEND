f7a0d79183791189c38a7fad5584b57c67a702ab
Mateusz Krawczykowicz <mateusz.krawczykowicz@student.vinci.be>
2021-12-16 20:53:31 +0100

    home page + css + rules

--- js-phaser-boilerplate/src/Components/Pages/HomePage.js
+++ js-phaser-boilerplate/src/Components/Pages/HomePage.js
@@ -6,7 +6,7 @@ const HomePage = () => {
   const pageDiv = document.querySelector("#page");
 
   
-  pageDiv.innerHTML = "<span>WATCHOUT!</span> <br>Les règles sont assez simple : <br> Vous controllez un personnage avec les flèches sur votre clavier. <br> Le but du jeux est de recuperer les plus de pieces sans se faire toucher par les différents véhicules qui se trouvent sur la route. Les pieces récuperées doivent etre ramenées dans le coffre qui se trouve au tout debut de la map pour que les prochaines pièces réaparessent. <br> Bonne chance !";
+  pageDiv.innerHTML = " <div class=\"watchout\"> <span>WATCHOUT!</span> </div> <div class=\"center\"> <br> <h1 style=\"color:#ffbf00\">Les règles du jeux : </h1><br> <h3>Vous controllez un personnage avec les flèches sur votre clavier. Le but du jeux est de recuperer les plus de pieces sans se faire toucher par les différents véhicules qui se trouvent sur la route. Les pieces récuperées doivent etre ramenées dans le coffrequi se trouve au tout debut de la map pour que les prochaines pièces réaparessent. Bonne chance ! </h3> </div> ";
 };
 
 export default HomePage;
