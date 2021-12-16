2a2feafd0a6ec4ba368117f61a9652a7955cc3b7
Mateusz Krawczykowicz <mateusz.krawczykowicz@student.vinci.be>
2021-12-16 21:46:32 +0100

    home page + trafic lights + about us

--- js-phaser-boilerplate/src/Components/Pages/AboutUsPage.js
+++ js-phaser-boilerplate/src/Components/Pages/AboutUsPage.js
@@ -1,6 +1,24 @@
 import { RedirectUrl } from "../Router/Router";
 
-let aboutUsPage = `<div id="container-fluid panneau-aboutus">
+let aboutUsPage = `
+
+  <div class=\"center\"> <br> <h1 style=\"color:#ffbf00\">About us :</h1><br>
+    <h4>
+    Bonjour,<br>
+    Nous sommes 4 étudiants en informatique de gestion,<br>
+    qui avons eu l'idée de faire un jeu pour notre projet de JavaScript.<br>     
+    Contact : <br>
+    GUNDUZ Dogukan<br>
+    dogukan.gunduz@student.vinci.be<br>
+    KRAWCZYKOWICZ Mateusz<br>
+    Mateusz.krawczykowicz@student.vinci.be<br>
+    </h4>
+  
+  </div>
+
+
+
+<div id="container-fluid panneau-aboutus">
   <ul class="cadre-panneau-aboutus">
     
      Bonjour,<br>
--- js-phaser-boilerplate/src/Components/Pages/HomePage.js
+++ js-phaser-boilerplate/src/Components/Pages/HomePage.js
@@ -6,7 +6,7 @@ const HomePage = () => {
   const pageDiv = document.querySelector("#page");
 
   
-  pageDiv.innerHTML = " <div class=\"watchout\"> <span>WATCHOUT!</span> </div> <div class=\"center\"> <br> <h1 style=\"color:#ffbf00\">Les règles du jeux : </h1><br> <h3>Vous controllez un personnage avec les flèches sur votre clavier. Le but du jeux est de recuperer les plus de pieces sans se faire toucher par les différents véhicules qui se trouvent sur la route. Les pieces récuperées doivent etre ramenées dans le coffrequi se trouve au tout debut de la map pour que les prochaines pièces réaparessent. Bonne chance ! </h3> </div> ";
+  pageDiv.innerHTML = " <div class=\"watchout\"> <span>WATCHOUT!</span> </div> <div class=\"center\"> <br> <h1 style=\"color:#ffbf00\">Les règles du jeux : </h1><br> <h3>Vous controllez un personnage avec les flèches sur votre clavier. Le but du jeux est de recuperer les plus de pieces sans se faire toucher par les différents véhicules qui se trouvent sur la route. Les pieces récuperées doivent etre ramenées dans le coffrequi se trouve au tout debut de la map pour que les prochaines pièces réaparessent. Bonne chance ! </h3> </div>      <div class=\"traffic-lights shape\"><div class=\"shadow\"></div><div class=\"light red\"></div><div class=\"light amber\"></div><div class=\"light green\"></div></div>     <div class=\"traffic-lights-r shape\"><div class=\"shadow\"></div><div class=\"light red\"></div><div class=\"light amber\"></div><div class=\"light green\"></div></div> ";
 };
 
 export default HomePage;
