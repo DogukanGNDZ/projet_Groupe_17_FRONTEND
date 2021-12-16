e3a0953c5653239a8d1d07747eb5ae83849345b7
Mateusz Krawczykowicz <mateusz.krawczykowicz@student.vinci.be>
2021-12-16 11:30:29 +0100

    home page

--- js-phaser-boilerplate/src/Components/Pages/HomePage.js
+++ js-phaser-boilerplate/src/Components/Pages/HomePage.js
@@ -5,7 +5,7 @@
 const HomePage = () => { 
   const pageDiv = document.querySelector("#page");
   
-  pageDiv.innerHTML = "Régle du jeux : <br> les regles sont assez simple vous controllez un personnage <br> et vous devez traversez une route remplis de vehicule de tout genre pour recuperer trois piece et les ramener dans votre coffre. ";
+  pageDiv.innerHTML = "Les règles sont assez simple : <br> Vous controllez un personnage avec les flèches sur votre clavier. <br> Le but du jeux est de recuperer les plus de pieces sans se faire toucher par les différents véhicules qui se trouvent sur la route. Les pieces récuperées doivent etre ramenées dans le coffre qui se trouve au tout debut de la map pour que les prochaines pièces réaparessent. <br> Bonne chance !";
 };
 
 export default HomePage;
