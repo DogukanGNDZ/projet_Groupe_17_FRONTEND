ab5e28149947eefee8f14400bc9eaec766850f9e
Mateusz Krawczykowicz <mateusz.krawczykowicz@student.vinci.be>
2021-12-17 14:24:44 +0100

    About us page + road

--- js-phaser-boilerplate/src/Components/Pages/AboutUsPage.js
+++ js-phaser-boilerplate/src/Components/Pages/AboutUsPage.js
@@ -1,24 +1,30 @@
 import { RedirectUrl } from "../Router/Router";
 
-let aboutUsPage = `
-
-  <div class=\"center\"> <br> <h1 style=\"color:#ffbf00\">About us :</h1><br>
+let aboutUsPage = ` 
+  <div class=\"container-about-us container_slide slide_down \"> <br> <h1 >About us :</h1><br>
     <h4>
     Bonjour,<br>
     Nous sommes 4 étudiants en informatique de gestion,<br>
     qui avons eu l'idée de faire un jeu pour notre projet de JavaScript.<br>     
-    Contact : <br>
+    Contact : <br><br>
     GUNDUZ Dogukan<br>
-    dogukan.gunduz@student.vinci.be<br>
-    KRAWCZYKOWICZ Mateusz<br>
-    Mateusz.krawczykowicz@student.vinci.be<br>
+    dogukan.gunduz@student.vinci.be <br><br>
+    KRAWCZYKOWICZ Mateusz <br>
+    mateusz.krawczykowicz@student.vinci.be <br><br>
+    DEVAUX Jérôme <br>
+    jerome.devaux@student.vinci.be <br><br>
+    FERRIERE Maxime <br>
+    maxime.ferriere@student.vinci.be <br>
     </h4>
-  
   </div>
 
+  <div class=\"infinite\">
+    <div class=\"shadow-road\"></div>
+  </div>
+`;
 
-
-<div id="container-fluid panneau-aboutus">
+/*
+ <div id="container-fluid panneau-aboutus">
   <ul class="cadre-panneau-aboutus">
     
      Bonjour,<br>
@@ -28,11 +34,10 @@ let aboutUsPage = `
      GUNDUZ Dogukan<br>
      dogukan.gunduz@student.vinci.be<br>
 
-     
+
 
   </ul>
-  </div>`;
-
+  </div> */
 const AboutUsPage = () => {
     const page = document.querySelector("#page");
     page.innerHTML = aboutUsPage;
